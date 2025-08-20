175a60634db97a586a82571133624e026fd5d788
MartinGV10 <mganen10@gmail.com>
2025-08-20 13:32:40 -0400

    Added insurance dialog

--- src/Reason.jsx
+++ src/Reason.jsx
@@ -1,10 +1,25 @@
 import './Reason.css'
 import Help from './Help'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useState, useRef } from 'react'
 
 function Reason() {
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
+    const dialogRef = useRef(null);
+
+    const handleAppointmentClick = () => {
+        setOpen(true);
+    };
+
+    const handleDialogButtonClick = () => {
+        setOpen(false);
+        navigate('./insurance');
+    };
+
     return (
         <>
+        {open && <div className="dialog-overlay"></div>}
         <div className="holder big-cont">
         <div className="main-wdw">
             <div className="instr-text relst">
@@ -22,12 +37,22 @@ function Reason() {
                     <p className="info">Provide number to NCH medical department at (239) 624-6567</p>
                 </div>
 
-                <Link to={'./insurance'}>
-                <div className="re-selection third">
+                <div className="re-selection third" onClick={handleAppointmentClick} style={{cursor: 'pointer'}}>
                     <h3 className="re-title">Appointment</h3>
-                    <p className="info">Click to continue appointment process</p>
+                    <p className="info" >Click to continue appointment process</p>
                 </div>
-                </Link>
+
+                {open && (
+                <dialog open ref={dialogRef} style={{zIndex: 1001}}>
+                    <div className="d-box">
+                        <div className="d-text">
+                            <h2 className="instr question">Have you verified the patient's insurance?</h2>
+                            <h3 className="re-title">You must check this before you continue</h3>
+                        </div>
+                        <button className="next" onClick={handleDialogButtonClick}>Yes</button>
+                    </div>
+                </dialog>
+                )}
 
             </div>
         </div>
